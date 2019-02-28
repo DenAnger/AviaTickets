fa7cefc363fe4e04d354de25f8c7b82db56143e8
Abramov Denis <denmiracle@mail.ru>
2019-02-28 14:46:46 +0700

    5
    1.  Добавил таббар. Добавил иконки поиска и карты.
    2. Реализовал поиск билетов по списку городов. Поиск появляктся после свайпа вниз по списку.

---
+++ AviaTickets/Controllers/TabBarController.m
@@ -0,0 +1,47 @@
+//
+//  TabBarController.m
+//  AviaTickets
+//
+//  Created by Denis Abramov on 27/02/2019.
+//  Copyright © 2019 Denis Abramov. All rights reserved.
+//
+
+#import "TabBarController.h"
+#import "MainViewController.h"
+#import "MapViewController.h"
+
+@interface TabBarController ()
+
+@end
+
+@implementation TabBarController
+
+//- (void)viewDidLoad {
+//    [super viewDidLoad];
+//}
+
+- (instancetype)init {
+    self = [super initWithNibName:nil bundle:nil];
+    if (self) {
+        self.viewControllers = [self createViewControllers];
+        self.tabBar.tintColor = [UIColor blackColor];
+    }
+    return self;
+}
+
+- (NSArray<UIViewController*> *)createViewControllers {
+    NSMutableArray<UIViewController*> *controllers = [NSMutableArray new];
+    MainViewController *mainViewController = [[MainViewController alloc] init];
+    mainViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Поиск" image:[UIImage imageNamed:@"search"] selectedImage:[UIImage imageNamed:@"search_selected"]];
+    UINavigationController *mainNavigationController = [[UINavigationController alloc] initWithRootViewController:mainViewController];
+    [controllers addObject:mainNavigationController];
+                                     
+                                     
+    MapViewController *mapViewController = [[MapViewController alloc] init];
+mapViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Карта цен" image:[UIImage imageNamed:@"map"] selectedImage:[UIImage imageNamed:@"map_selected"]];
+    UINavigationController *mapNavigationController = [[UINavigationController alloc] initWithRootViewController:mapViewController];
+    [controllers addObject:mapNavigationController];
+    return controllers;
+}
+
+@end
